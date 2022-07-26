2ef5efeca4144b16313fce54df622cd7889093ad
Nourhene Maaouia <nourhene.maaouia@esprit.tn>
2022-07-27 00:52:05 +0200

    first commit

---
+++ GrosysBackend/src/main/java/com/example/grosysbackend/GrosysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.grosysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrosysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrosysBackendApplication.class, args);
+    }
+
+}
---
+++ GrosysBackend/src/test/java/com/example/grosysbackend/GrosysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grosysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrosysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
